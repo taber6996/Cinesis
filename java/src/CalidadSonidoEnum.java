ee4488b9c8a99889a696edb88886f0df67546d0a
alvant01 <alvant01@ucm.es>
2018-04-27 16:47:12 +0200

    codigo java

---
+++ java/src/CalidadSonidoEnum.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+/** 
+ * <!-- begin-UML-doc -->
+ * <!-- end-UML-doc -->
+ * @author usuario_local
+ * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public enum CalidadSonidoEnum {
+}
