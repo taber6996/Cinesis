7b2d6ec6f2e87fb2d33f745201ee338e9780e83e
Alejandro Zorita <alejandro.zorita@medssocial.com>
2018-04-29 12:57:32 +0200

    Finalizamos conexión

--- CinesisContainer/src/main/java/com/master/cinesis/configuration/DataBaseConfiguration.java
+++ CinesisContainer/src/main/java/com/master/cinesis/configuration/DataBaseConfiguration.java
@@ -1,10 +1,14 @@
 package com.master.cinesis.configuration;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -17,6 +21,7 @@ public class DataBaseConfiguration {
 		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
 		sessionFactoryBean.setDataSource(dataSource());
 		sessionFactoryBean.setPackagesToScan("com.master.cinesis.model");
+		sessionFactoryBean.setHibernateProperties(hibernateProperties());
 		
 		return null;
 	}
@@ -31,5 +36,23 @@ public class DataBaseConfiguration {
 		dataSource.setPassword("Yh0q1b#1");
 		
 		return dataSource;
+	}
+	
+	
+	public Properties hibernateProperties(){
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", "hibernate.dialect");
+		properties.put("show_sql", "true");
+		
+		return properties;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(){
+		HibernateTransactionManager hiberanteTansactionManager = new HibernateTransactionManager();
+		hiberanteTansactionManager.setSessionFactory(sessionFactory().getObject());
+		return hiberanteTansactionManager;
+		
 	}
 }
