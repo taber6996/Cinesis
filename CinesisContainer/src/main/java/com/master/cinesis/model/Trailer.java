2b5f3d100f17a3b27bba772ab525dfff954e42e3
jaimepal <jaimepal@ucm.es>
2018-05-04 18:01:29 +0200

    Add files via upload

---
+++ CinesisContainer/src/main/java/com/master/cinesis/model/Trailer.java
@@ -0,0 +1,55 @@
+package com.master.cinesis.model;
+
+import javax.persistence.*;
+
+public class Trailer {
+	
+	private Integer idTrailer;
+	
+	private Pelicula idPelicula;
+	
+	private String titulo;
+	
+	private String descripcion;
+	
+	private Pelicula peliculas;
+
+	public void crearTrailer(String idAministrador, Integer duracion,
+			String titulo, String descripcion) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	public void eliminarTrailer(String idAdministrador, String idTrailer) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	
+	public void mostrarTrailer(String idTrailer) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	public void modificarTrailer(String idAministrador, Integer duracion,
+			String titulo, String descripcion, String idTrailer) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	
+	public void mostrarFormulario() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+}
