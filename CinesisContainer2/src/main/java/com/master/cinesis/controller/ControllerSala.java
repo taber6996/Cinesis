ce63cbf0b794e9b881a2f5e1cb643d328e263939
alvant01 <alvant01@ucm.es>
2018-05-18 17:58:10 +0200

    Update ControllerSala.java

--- CinesisContainer2/src/main/java/com/master/cinesis/controller/ControllerSala.java
+++ CinesisContainer2/src/main/java/com/master/cinesis/controller/ControllerSala.java
@@ -1,6 +1,5 @@
 package com.master.cinesis.controller;
 
-import java.lang.reflect.Array;
 import java.util.Iterator;
 import java.util.List;
 
@@ -13,12 +12,9 @@ public class ControllerSala implements ControllerInter {
 	private String id = "ContSala";
 
 	
-	
-	
-	
-	
-	@Override
-	public void insert(List<String> lista) {
+		
+	public void insert(List<String> lista)
+	{
 		Iterator<String> it = lista.iterator();
 		
 		Sala S = new Sala(Integer.parseInt(it.next()),Integer.parseInt(it.next()),Integer.parseInt(it.next()),
@@ -26,29 +22,30 @@ public class ControllerSala implements ControllerInter {
 				stringToEnum(it.next()),Boolean.parseBoolean(it.next()));
 		
 		S.crearSala();
-	
-		
-		
-		
 	}
-
 	@Override
 	public void Delete(Integer id) {
-		//Sala S = new Sala();
+		Sala S = new Sala(id, null, null, null, null, null, null, false);
 		
-		
+		S.eliminarSala(id);
 	}
 
 	@Override
-	public void read(Integer id) {
-		// TODO Auto-generated method stub
+	public List<String> read(Integer id) {
+		Sala S = new Sala(id, null, null, null, null, null, null, false);
+		S.mostrarSala(id);
 		
+		return S.objectToList();
 	}
 
 	@Override
 	public void modify(List<String> lista) {
-		// TODO Auto-generated method stub
+		Iterator<String> it = lista.iterator();
 		
+		Sala S = new Sala(Integer.parseInt(it.next()),Integer.parseInt(it.next()),Integer.parseInt(it.next()),
+				Integer.parseInt(it.next()),Integer.parseInt(it.next()),Integer.parseInt(it.next()),
+				stringToEnum(it.next()),Boolean.parseBoolean(it.next()));
+		S.modificarSala();
 	}
 
 	@Override
@@ -67,7 +64,4 @@ public class ControllerSala implements ControllerInter {
 		return null;
 	
 	}
-
-
-
 }
