136616f3c5c2ea7e627b06fb6240f091732e5a01
jaimepal <jaimepal@ucm.es>
2018-05-18 15:18:54 +0200

    Controller con patron command

--- CinesisContainer2/src/main/java/com/master/cinesis/controller/Controller.java
+++ CinesisContainer2/src/main/java/com/master/cinesis/controller/Controller.java
@@ -1,22 +1,9 @@
 package com.master.cinesis.controller;
 
-import java.lang.reflect.Array;
-import java.util.List;
+public class Controller {
+	
+	private ControllerInter controlador;
+	
+	private ControllerParser controladorParser;
 
-public interface  Controller {
-	
-	//private List<String> lista;
-
-	public void insert(String [] lista);
-	
-	public void Delete(Integer id);
-	
-	public void read(Integer id);
-	
-	public void modify(List<String> lista);
-	
-	public Enum stringToEnum(String stringEnum);
-	
-	
-	
 }
--- CinesisContainer2/src/main/java/com/master/cinesis/controller/ControllerSala.java
+++ CinesisContainer2/src/main/java/com/master/cinesis/controller/ControllerSala.java
@@ -1,14 +1,16 @@
 package com.master.cinesis.controller;
 
 import java.lang.reflect.Array;
+import java.util.Iterator;
 import java.util.List;
+
 
 import com.master.cinesis.model.Sala;
 import com.master.cinesis.model.CalidadSonidoEnum;
 
-public class ControllerSala implements Controller {
+public class ControllerSala implements ControllerInter {
 	
-	
+	private String id = "ContSala";
 
 	
 	
@@ -16,14 +18,15 @@ public class ControllerSala implements Controller {
 	
 	
 	@Override
-	public void insert(String [] lista) {
+	public void insert(List<String> lista) {
+		Iterator<String> it = lista.iterator();
 		
-		
-		Sala S = new Sala(Integer.parseInt(lista[0]),Integer.parseInt(lista[1]),Integer.parseInt(lista[2]),
-				Integer.parseInt(lista[3]),Integer.parseInt(lista[4]),Integer.parseInt(lista[5]),
-				stringToEnum(lista[6]),Boolean.parseBoolean(lista[7]));
+		Sala S = new Sala(Integer.parseInt(it.next()),Integer.parseInt(it.next()),Integer.parseInt(it.next()),
+				Integer.parseInt(it.next()),Integer.parseInt(it.next()),Integer.parseInt(it.next()),
+				stringToEnum(it.next()),Boolean.parseBoolean(it.next()));
 		
 		S.crearSala();
+	
 		
 		
 		
@@ -31,7 +34,8 @@ public class ControllerSala implements Controller {
 
 	@Override
 	public void Delete(Integer id) {
-		// TODO Auto-generated method stub
+		//Sala S = new Sala();
+		
 		
 	}
 
@@ -51,6 +55,17 @@ public class ControllerSala implements Controller {
 	public CalidadSonidoEnum stringToEnum(String stringEnum) {
 		CalidadSonidoEnum ec = CalidadSonidoEnum.valueOf(stringEnum);
 		return ec;
+	}
+
+	@Override
+	public ControllerInter parse(List<String> lista) {
+		Iterator<String> it = lista.iterator();
+		
+		if(it.next() == this.id)
+			return this;
+		
+		return null;
+	
 	}
 
 
