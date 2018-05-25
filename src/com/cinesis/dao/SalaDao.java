940578c3e9e14e50ee42004d001c49815c6df9ab
jaimepal <jaimepal@ucm.es>
2018-05-25 18:29:07 +0200

    Saladao

--- src/com/cinesis/dao/SalaDao.java
+++ src/com/cinesis/dao/SalaDao.java
@@ -1,6 +1,8 @@
 package com.cinesis.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
@@ -8,6 +10,8 @@ import java.util.List;
 
 import com.cinesis.model.Sala;
 import com.cinesis.servicios.Conexion;
+
+
 
 public class SalaDao extends AbstractSession{
 	
@@ -18,13 +22,13 @@ public class SalaDao extends AbstractSession{
 		 try { 
    		  Conexion cnx = new Conexion();
    		   Connection conn = cnx.obtener();
-   		   Statement stmt = conn.createStatement();
+   		  // Statement stmt = conn.createStatement();
    		   String query = "INSERT INTO sala(num_filas,num_columnas,num_asientos_vip,num_asientos_minusvalidos,calidad_sonido,3d)  Values( '" + sala.getNumFilas() +" ',' "+ sala.getNumColumnas() +"',' "+ sala.getNumAsientosVIP()+" ',' "+ sala.getNumAsientosMinusvalidos() +" ',' "+ sala.getCalidadSonido() +" ',' "+ booleanToInt(sala.get_3D()) +"')";
    		   java.sql.PreparedStatement preparedStmt = conn.prepareStatement(query);
    		   boolean b = preparedStmt.execute();
    		   System.out.println(b);
-   		   stmt.close();
-   		   conn.close();
+   		  // stmt.close();
+   		   //conn.close();
    		  }catch(ClassNotFoundException | SQLException e) {
    			  System.out.println("nop" + e);
    			  
@@ -42,19 +46,82 @@ public class SalaDao extends AbstractSession{
 	
 	public void deleteSalaById(Integer sala_id){
 		
+		 try { 
+	   		  Conexion cnx = new Conexion();
+	   		   Connection conn = cnx.obtener();
+	   		  // Statement stmt = conn.createStatement();
+	   		   String query = "DELETE FROM sala  WHERE id = '" + sala_id + "'";
+	   		   java.sql.PreparedStatement preparedStmt = conn.prepareStatement(query);
+	   		   boolean b = preparedStmt.execute();
+	   		   System.out.println(b);
+	   		  // stmt.close();
+	   		  // conn.close();
+	   		  }catch(ClassNotFoundException | SQLException e) {
+	   			  System.out.println("nop" + e);
+	   			  
+	   		  }
+		
+		
+		
+		
 	}
 	
 	public void updateSala(Sala sala){
+		
+		
+		 try { 
+	   		  Conexion cnx = new Conexion();
+	   		   Connection conn = cnx.obtener();
+	   		  // Statement stmt = conn.createStatement();
+	   		   String query = "UPDATE sala S SET S.id = '"+ sala.getIdSala() +"', S.num_filas ='" + sala.getNumFilas() +" ', S.num_columnas =' "+ sala.getNumColumnas() +"', S.num_asientos_vip =' "+ sala.getNumAsientosVIP()+" ', S.num_asientos_minusvalidos =' "+ sala.getNumAsientosMinusvalidos() +" ', S.calidad_sonido =  ' "+ sala.getCalidadSonido() +" ',S.3d =' "+ booleanToInt(sala.get_3D()) +"' ";
+	   		   java.sql.PreparedStatement preparedStmt = conn.prepareStatement(query);
+	   		   boolean b = preparedStmt.execute();
+	   		   System.out.println(b);
+	   		   //stmt.close();
+	   		   //conn.close();
+	   		  }catch(ClassNotFoundException | SQLException e) {
+	   			  System.out.println("nop" + e);
+	   			  
+	   		  }
 			}
+	        
+	        
+	        
+	        
+	        
 	
 	public List<Sala> findAllSala(){
 		return null;	}
 	
-	public Sala findById(Integer sala_id){
-		return null;	}
 	
-	public Sala findByName(String sala_nombre) {
-		return null;	}
+	
+	
+	
+	
+	public ResultSet findById(Integer sala_id){
+		ResultSet rs = null;
+		try { 
+  		  Conexion cnx = new Conexion();
+  		   Connection conn = cnx.obtener();
+  		   //Statement stmt = conn.createStatement();
+  		   String query = "SELECT * From sala S Where S.id = '" + sala_id + "'";
+  		   java.sql.PreparedStatement preparedStmt = conn.prepareStatement(query);
+  		   rs = preparedStmt.executeQuery();
+  		   if (rs.getRow() == 1)
+  		   {
+  		   }
+  		 //  stmt.close();
+  		  }catch(ClassNotFoundException | SQLException e) {
+  			  System.out.println("nop" + e);
+  			  
+  		  }
+		return rs;
+		
+		
+	
+	}
+	
+	
 }
 
 
