e3230cc3ab761764661d9d4be4f9b326d2cc77b6
Alejandro Zorita <alejandro.zorita@medssocial.com>
2018-05-26 17:29:16 +0200

    conection .jar

--- src/com/cinesis/servicios/Conexion.java
+++ src/com/cinesis/servicios/Conexion.java
@@ -3,26 +3,29 @@ import java.sql.*;
 
 //import org.apache.log4j.Logger;
 public class Conexion {
-    
-    
-    
+     
    private static Connection cnx = null;
-   
-   
-   
-   
    
    public  Connection obtener() throws SQLException, ClassNotFoundException {
       if (cnx == null) {
          try {
         	Class.forName("com.mysql.jdbc.Driver");
-            cnx = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:8889/cinesis", "root", "root");
+        	
+        	String cadcon="jdbc:mysql://localhost:8889/cinesis";
+        	String user="root";
+        	String password="root";
+        	
+            cnx = DriverManager.getConnection(cadcon, cadcon, password);
          } catch (SQLException ex) {
-        	ex.printStackTrace();
-            throw new SQLException(ex); 
+        	System.out.println("Error sql: "+ex.getMessage());
+        	//ex.printStackTrace();
+            //throw new SQLException(ex); 
          } catch (ClassNotFoundException ex) {
+        	 System.out.println("Error class: "+ex.getMessage());
             throw new ClassCastException(ex.getMessage());
-         }
+         } catch (Exception ex){
+    	   System.out.println("Error: "+ex.getMessage());
+    	}
       }
       return cnx;
    }
