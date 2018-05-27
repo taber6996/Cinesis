11ee5bb45bf79b653d0635d7e5cfa8499d6d3d81
alvant01 <alvant01@ucm.es>
2018-05-27 03:41:12 +0200

    Correccion findAllPeliculas()

--- src/com/cinesis/dao/PeliculaDao.java
+++ src/com/cinesis/dao/PeliculaDao.java
@@ -81,21 +81,16 @@ public class PeliculaDao extends AbstractSession{
   		   Connection conn = cnx.obtener();
   		  // Statement stmt = conn.createStatement();
   		   String query = "SELECT * From peliculas";
-
   		   java.sql.PreparedStatement preparedStmt = conn.prepareStatement(query);
   		   rs = preparedStmt.executeQuery();
-  		 System.out.println(rs.next());
-  		   while(rs.isLast())
-  		   {
-  			 list.add(setDatosQuery(rs));
-  			 rs.next();
-  		   }
-  		 //  stmt.close();
-  		 //  conn.close();
+
+  			 while(rs.next())
+  			 {
+  				 list.add(setDatosQuery(rs));
+  			 }
   		  }catch(ClassNotFoundException | SQLException e) {
   			  System.out.println("nop" + e);
   		  }
-		
 		return list;
 	}
 	
